6155786146bf2abfc55dc4f5acaaa807a78c566f
David-Voss <124778797+David-Voss@users.noreply.github.com>
2025-03-15 13:46:27 +0100

    ImagePropertiesWindow can now be close by hitting the EXC button on the keyboard.

--- src/main/java/controller/components/ImagePropertiesController.java
+++ src/main/java/controller/components/ImagePropertiesController.java
@@ -3,7 +3,11 @@ package controller.components;
 import model.ImagePropertiesModel;
 import view.components.ImagePropertiesView;
 
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 
 public class ImagePropertiesController {
@@ -36,6 +40,13 @@ public class ImagePropertiesController {
             updateSizeValues();
             view.dispose();
         });
+
+        // **ESC-Taste schließt das Fenster**
+        view.getRootPane().registerKeyboardAction(
+                e -> view.dispose(),
+                KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+                JComponent.WHEN_IN_FOCUSED_WINDOW
+        );
 
         // Abbrechen-Button schließt das Fenster
         view.getCancelButton().addActionListener(e -> view.dispose());
